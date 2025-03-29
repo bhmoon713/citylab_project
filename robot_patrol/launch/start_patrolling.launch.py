b225852fd9fbfcd61a9098fd9db27fe7653d7081
bhmoon <bhmoon713@gmail.com>
2025-03-29 04:25:49 +0000

    checking /scan topic information, 660 points, full angle, counter clockwise starting from back, created RVIS parameter

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,14 +1,31 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    # Path to your RViz config file
+    rviz_config_path = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rviz',
+        'patrol.rviz'
+    )
+
     return LaunchDescription([
         Node(
             package='robot_patrol',
             executable='robot_patrol_node',
             name='robot_patrol_node',
             output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path]
         )
     ])
--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -16,28 +16,33 @@ public:
 private:
   void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
   {
+    // float right = msg->ranges[msg->ranges.size()* 1 / 4];
+    // float front_right = msg->ranges[msg->ranges.size()* 1.5 / 4];
     // float front = msg->ranges[msg->ranges.size() / 2];
-    // float left = msg->ranges[msg->ranges.size() * 1 / 4];
-    // float right = msg->ranges[msg->ranges.size()* 3 / 4];
-    float left = msg->ranges[0];
-    float front_left = msg->ranges[180];
-    float front = msg->ranges[360];
-    float front_right = msg->ranges[540];
-    float right = msg->ranges[719];
- RCLCPP_INFO(this->get_logger(), "Laser Readings → Front: %.2f m | Left: %.2f m | Right: %.2f m", front, left, right);
+    // float front_left = msg->ranges[msg->ranges.size() * 2.5 / 4];
+    // float left = msg->ranges[msg->ranges.size() * 3 / 4]; 
+    
+    float right = msg->ranges[164];
+    float front_right = msg->ranges[247];
+    float front = msg->ranges[329];
+    float front_left = msg->ranges[412];
+    float left = msg->ranges[494];
+
+    RCLCPP_INFO(this->get_logger(), "Laser Size: %ld", msg->ranges.size());
+    RCLCPP_INFO(this->get_logger(), "Laser Readings → Front: %.2f m | Left: %.2f m | Right: %.2f m", front, left, right);
     auto cmd = geometry_msgs::msg::Twist();
 
-  if (front > 1.0 && front_left > 1.0 && front_right > 1.0) {
+  if (front > 0.3 && front_left > 0.3 && front_right > 0.3) {
     // ✅ No obstacle: Move forward
-    cmd.linear.x = 0.5;
+    cmd.linear.x = 0.1;
     cmd.angular.z = 0.0;
   } 
-  else if (front < 1.0 || right < 1.0 || front_right < 1.0 ) {
+  else if (front < 0.3 || right < 0.3 || front_right < 0.3 ) {
     // ✅ Obstacle in front OR right: Turn left
     cmd.linear.x = 0.0;
     cmd.angular.z = 0.5;
   }
-  else if (left < 1.0 ) {
+  else if (left < 0.3 ) {
     // ✅ Obstacle in left: Turn right
     cmd.linear.x = 0.0;
     cmd.angular.z = -0.5;
