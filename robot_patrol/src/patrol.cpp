9f4ad4011908e30bc19efbe4d6e0f55c9f5ad15d
bhmoon <bhmoon713@gmail.com>
2025-04-03 09:36:31 +0000

    created new branch and saved file.. very similar code to sim

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -28,8 +28,8 @@ private:
   float min_direction_; // Angle to closest obstacle
   float max_value_;     // Max distance between 164~494
   float max_direction_; // Angle to max distance
-  float left_= 0.0;
-  float front_= 0.0;
+  float left_ = 0.0;
+  float front_ = 0.0;
   float right_ = 0.0;
 
   // === Callbacks ===
@@ -44,65 +44,50 @@ private:
     RCLCPP_INFO(this->get_logger(),
                 "📡 Laser size: %zu | Left %.2f Front %.2f Right: %.2f ",
                 msg->ranges.size(), left_, front_, right_);
-
-    // --- Min value in full range ---
-    // auto min_it = std::min_element(msg->ranges.begin() + 164,
-    // msg->ranges.begin() + 494); min_value_ = *min_it; int min_index =
-    // std::distance(msg->ranges.begin(), min_it); min_direction_ = (min_index -
-    // 329) * 6.28 / 660;
-
+    // Get min distance value at front side (45 degree to 135 degree). get
+    // direction.
     auto min_it = std::min_element(msg->ranges.begin() + scanN * 1.5 / 4,
                                    msg->ranges.begin() + scanN * 2.5 / 4);
     min_value_ = *min_it;
     int min_index = std::distance(msg->ranges.begin(), min_it);
     min_direction_ = (min_index - scanN / 2) * 6.28 / scanN;
 
-    // --- Max value in specific range 164~494 ---
-    // auto max_it = std::max_element(msg->ranges.begin() + 164,
-    // msg->ranges.begin() + 494); max_value_ = *max_it; int max_index =
-    // std::distance(msg->ranges.begin(), max_it); max_direction_ = (max_index -
-    // 329) * 6.28 / 660;
-
+    // Get max distance value at front side (0 degree to 180 degree). get
+    // direction.
     auto max_it = std::max_element(msg->ranges.begin() + scanN / 4,
                                    msg->ranges.begin() + scanN * 3 / 4);
     max_value_ = *max_it;
     int max_index = std::distance(msg->ranges.begin(), max_it);
     max_direction_ = (max_index - scanN / 2) * 6.28 / scanN;
-
-    // --- Print for debug ---
-    // RCLCPP_INFO(this->get_logger(), "📡 Laser size: %ld | Min: %.2f at %.2f
-    // rad | Max: %.2f at %.2f rad",
-    //             msg->ranges.size(), min_value_, min_direction_, max_value_,
-    //             max_direction_);
   }
 
-    void checkvicinity(geometry_msgs::msg::Twist &cmd) {
-        if (min_value_ < 0.30 && min_direction_ < 0) {
-            cmd.linear.x = 0.05;
-            cmd.angular.z = 0.75;
-            RCLCPP_INFO(this->get_logger(), "There is something on right");
-        } else if (min_value_ < 0.25 && min_direction_ > 0) {
-            cmd.linear.x = 0.05;
-            cmd.angular.z = -0.75;
-            RCLCPP_INFO(this->get_logger(), "There is something on left");
-        }
-
+  void checkvicinity(geometry_msgs::msg::Twist &cmd) {
+    if (min_value_ < 0.30 && min_direction_ < 0) {
+      cmd.linear.x = 0.025;
+      cmd.angular.z = 0.75;
+      RCLCPP_INFO(this->get_logger(), "There is something on right");
+    } else if (min_value_ < 0.25 && min_direction_ > 0) {
+      cmd.linear.x = 0.025;
+      cmd.angular.z = -0.75;
+      RCLCPP_INFO(this->get_logger(), "There is something on left");
     }
+  }
 
   void timerCallback() {
     auto cmd = geometry_msgs::msg::Twist();
 
     // === Obstacle Avoidance Logic ===
     if (front_ > 0.35) {
-        cmd.linear.x = 0.1;
-        cmd.angular.z = 0.0;
-        checkvicinity(cmd);
-        RCLCPP_INFO(this->get_logger(), "I am going forward");
+      cmd.linear.x = 0.1;
+      cmd.angular.z = 0.0;
+      checkvicinity(cmd);
+      RCLCPP_INFO(this->get_logger(), "I am going forward");
     } else {
-        cmd.linear.x = 0.1;
-        cmd.angular.z = max_direction_ / 2;
-        checkvicinity(cmd);
-        RCLCPP_INFO(this->get_logger(), "I am turning to longest distance direction");
+      cmd.linear.x = 0.1;
+      cmd.angular.z = max_direction_ / 2;
+      checkvicinity(cmd);
+      RCLCPP_INFO(this->get_logger(),
+                  "I am turning to longest distance direction");
     }
     cmd_pub_->publish(cmd);
   }
